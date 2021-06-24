867241f59a3c5154f82ae235d4315266ec69817a
Diogo Carasco da Silva <carasco@gmail.com>
2021-06-24 02:08:18 -0300

    Messages customization

--- config/database.go
+++ config/database.go
@@ -9,18 +9,20 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+	"github.com/diogocarasco/golang-api-template/utils"
 )
 
 // InitializeDB starts the database  connection and appends it to the gin context
 func InitializeDB() (*mongo.Client, error) {
 
-	fmt.Print("Initializing database -> ")
+	fmt.Print(utils.Blue + "Initializing database -> " + utils.Reset)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	// Connecting to MongoDB
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"",
+		"mongodb+srv://golangapi:golangapi@cluster0.92n6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 	))
 	if err != nil {
 		log.Fatal(err)
@@ -28,12 +30,11 @@ func InitializeDB() (*mongo.Client, error) {
 		//Check the connection
 		err := client.Ping(ctx, readpref.Primary())
 		if err != nil {
+			fmt.Println(utils.Red + "Database connection failed!" + utils.Reset)
 			cancel()
 		}
-		fmt.Println("Database connected!")
+		fmt.Println(utils.Green + "Database connected!" + utils.Reset)
 	}
-	//var db type *utils.Database{client,ctx}
-	//db := utils.Database{dbclient: client, dbcontext: ctx}
 
 	return client, err
 
