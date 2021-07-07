399b744a513cd003f6b6b541da63d3a02dbe0e24
Diogo Carasco da Silva <carasco@gmail.com>
2021-07-07 01:32:02 -0300

    Adjusts for swagger documentation tool  -- routes.go     + parameter for insert route

--- main.go
+++ main.go
@@ -4,6 +4,12 @@ import (
 	"github.com/diogocarasco/golang-api-template/server"
 )
 
+// @title Go Gin-Gonic Example API
+// @version 1.0
+// @description A Gin-Gonic API template for general purposes
+// @contact.name Diogo Carasco
+// @contact.url http://www.github.com/diogocarasco
+
 func main() {
 
 	app := server.GetServer()
--- server/routes.go
+++ server/routes.go
@@ -3,6 +3,9 @@ package server
 import (
 	"github.com/diogocarasco/golang-api-template/controllers"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	_ "github.com/swaggo/gin-swagger/example/basic/docs"
 )
 
 //SetRoutes Sets the API routes for gin engine
@@ -11,5 +14,9 @@ func SetRoutes(server *gin.Engine) {
 	server.GET("/", controllers.Hello)
 	server.POST("/insert", controllers.Insert)
 	server.POST("/insertmany", controllers.InsertMany)
+	server.GET("/find/:id", controllers.Find)
+
+	url := ginSwagger.URL("http://localhost:8080/swagger/doc.json") // The url pointing to API definition
+	server.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 }
